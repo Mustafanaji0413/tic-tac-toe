84373fdf8c17b8c4b456b6dfb342d844347a8953
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-11-09 14:13:25 +0000

    Comment out how the game will work

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# tic tac toe
+
+"""
+tic tac toe board
+[
+    [-, -, -,],
+    [-, -, -,],
+    [-, -, -,]
+]
+
+user_input -> somthing between 1-9
+if they input somthing else say: Wrong input, try again!
+check if the user input has already been taken
+add it to the board
+check if somone won: check rows, colums and diagonals
+jump between users after each correct move
+"""
+
