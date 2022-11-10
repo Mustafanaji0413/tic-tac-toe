ceff7de62dce7bac9850726c3700f2a711bff3ee
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-11-10 22:34:03 +0000

    Check if user input is free to play

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def print_board(board):
             print(f"{slot}  ", end="")
         print()
              
-print_board(board)
+
 
 def quit(user_input):
     if user_input == "q": 
@@ -55,9 +55,30 @@ def bounds(user_input):
         return False
     else: return True    
 
+def istaken(coords, board):
+    row = coords[0]
+    col = coords[1]
+    if board[row][col] != "-":
+        print("This posotion is already taken, try again.")
+        return True
+    else: return False    
+
+def coordinates(user_input):
+    row = int(user_input / 3)
+    col = user_input 
+    if col > 2: col = int(col % 3)
+    return (row,col)
+
 while True:
+    print_board(board)
     user_input = input("Please choose a position 1 through 9 or enter \"q\" to quit:")
     if quit(user_input): break
     if not check_input(user_input):
         print("Please try again!")
+        continue
+    user_input = int(user_input) - 1
+    coords = coordinates(user_input)
+    board[0][0] = "x"
+    if istaken(coords, board):
+        print("Please try again!!!")
         continue
